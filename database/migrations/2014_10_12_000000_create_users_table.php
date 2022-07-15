5f2a7ba622153cfe829b5ffdae85780daaab9fc9
Shakeel Ahmad <shakeel2717@gmail.com>
2022-07-15 02:55:28 -0700

    remove unsed layouts

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('status')->default(true);
+            $table->boolean('suspend')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
