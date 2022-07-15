b381a3b5d04f5a6836efb69801badbf824985ef7
Shakeel Ahmad <shakeel2717@gmail.com>
2022-07-15 02:51:02 -0700

    login page complete

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,40 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+@extends('layouts.auth')
+@section('form')
+    <form class="js-validate" method="POST" action="{{ route('login') }}">
+        @csrf
+        <div class="text-center mb-5">
+            <h1 class="display-4">Welcome Back!</h1>
+            <p>Don't have an account yet? <a href="{{ route('register') }}">Sign up here</a>
+            </p>
+        </div>
+        <hr>
+        <div class="form-row">
+            <div class="col-sm-12">
+                <div class="js-form-message form-group">
+                    <label class="input-label" for="email">Your email</label>
+                    <input type="email" class="form-control form-control-lg" name="email" id="email"
+                        placeholder="Markwilliams@example.com" required>
+                </div>
             </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+        </div>
+        <div class="form-row">
+            <div class="col-sm-12">
+                <div class="js-form-message form-group">
+                    <label class="input-label" for="password">Type Password</label>
+                    <input type="password" class="form-control form-control-lg" name="password" id="password"
+                        placeholder="**********" required>
+                </div>
             </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+        </div>
+        <div class="js-form-message form-group">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="termsCheckbox" name="termsCheckbox">
+                <label class="custom-control-label text-muted" for="termsCheckbox">
+                    Stay Logged In
                 </label>
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+        </div>
+        <button type="submit" class="btn btn-lg btn-block btn-primary mb-2">Sign In</button>
+        <a class="btn btn-block btn-link" href="{{ route('password.request') }}">Forgot Password?</a>
+    </form>
+@endsection
