7e27348373f0748dd7b36af030ca176fddc3c6eb
Shakeel Ahmad <shakeel2717@gmail.com>
2022-07-17 05:16:12 -0700

    Customer can See All Rmas

--- resources/views/user/customer/show.blade.php
+++ resources/views/user/customer/show.blade.php
@@ -9,7 +9,8 @@
 
                     <div class="row align-items-center gx-2">
                         <div class="col">
-                            <span class="js-counter display-4 text-dark" data-value="24">0</span>
+                            <span class="js-counter display-4 text-dark"
+                                data-value="24">{{ $customer->customerRmas->count() }}</span>
                         </div>
                     </div>
                 </div>
@@ -22,7 +23,8 @@
 
                     <div class="row align-items-center gx-2">
                         <div class="col">
-                            <span class="js-counter display-4 text-dark" data-value="24">0</span>
+                            <span class="js-counter display-4 text-dark"
+                                data-value="24">{{ $customer->customerRefunds->sum('amount') }}</span>
                         </div>
                     </div>
                 </div>
@@ -31,11 +33,12 @@
         <div class="col-sm-6 col-lg-3 mb-3 mb-lg-5">
             <div class="card h-100">
                 <div class="card-body">
-                    <h6 class="card-subtitle mb-2">Total Refunded</h6>
+                    <h6 class="card-subtitle mb-2">Total Inventories</h6>
 
                     <div class="row align-items-center gx-2">
                         <div class="col">
-                            <span class="js-counter display-4 text-dark" data-value="24">0</span>
+                            <span class="js-counter display-4 text-dark"
+                                data-value="24">{{ $customer->customerInventories->sum('price') }}</span>
                         </div>
                     </div>
                 </div>
@@ -48,7 +51,9 @@
 
                     <div class="row align-items-center gx-2">
                         <div class="col">
-                            <span class="js-counter display-4 text-dark" data-value="24">0</span>
+                            <span class="js-counter display-4 text-dark"
+                                data-value="24">{{ $customer->customerInventories->sum('price') - $customer->customerRefunds->sum('amount') }}
+                            </span>
                         </div>
                     </div>
                 </div>
@@ -59,23 +64,65 @@
         <div class="col-lg-4">
             <x-profile-box-card :user="$customer" />
         </div>
+        <div class="col-lg-8 mb-3 mb-lg-5">
+            <div class="card h-100">
+                {{-- <div class="card-header">
+                    <ul class="nav nav-segment" id="navTab1" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" id="nav-resultTab1" data-toggle="pill" href="#lnventoryData"
+                                role="tab" aria-controls="lnventoryData" aria-selected="true">lnventory</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="nav-htmlTab1" data-toggle="pill" href="#refundData" role="tab"
+                                aria-controls="refundData" aria-selected="false">Refund</a>
+                        </li>
+                    </ul>
+                </div> --}}
+                <div class="tab-content" id="navTabContent1">
+                    <div class="tab-pane fade p-4 active show" id="lnventoryData" role="tabpanel"
+                        aria-labelledby="nav-resultTab1">
+                        <div class="">
+                            <div class="table-responsive">
+                                <table
+                                    class="table table-borderless table-thead-bordered table-nowrap table-align-middle card-table">
+                                    <thead class="thead-light">
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">Total Refund</th>
+                                            <th scope="col">Refunded</th>
+                                            <th scope="col">Refund Due</th>
+                                            <th scope="col">Status</th>
+                                            <th scope="col">Date</th>
+                                        </tr>
+                                    </thead>
 
-        <div class="col-lg-8">
-            <div class="card">
-                <div class="card-header">
-                    <div class="row justify-content-between align-items-center flex-grow-1">
-                        <div class="col-12 col-md">
-                            <div class="d-flex justify-content-between align-items-center">
-                                <h5 class="card-header-title">All RMAs List</h5>
+                                    <tbody>
+                                        @forelse ($customer->customerRmas as $rma)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>{{ number_format($rma->customer->customerInventoriesRmas($rma->id)->sum('price'), 2) }}
+                                                </td>
+                                                <td>{{ number_format($rma->customer->customerRefundsRmas($rma->id)->sum('amount'), 2) }}
+                                                </td>
+                                                <td>{{ number_format($rma->customer->customerInventoriesRmas($rma->id)->sum('price') - $rma->customer->customerRefundsRmas($rma->id)->sum('amount'), 2) }}
+                                                </td>
+                                                <td class="text-uppercase"> <span
+                                                        class="badge badge-primary">{{ $rma->status }}</span></td>
+                                                <td>{{ $rma->created_at->diffForHumans() }}</td>
+                                            </tr>
+                                        @empty
+                                            <p class="text-center">No Record Found</p>
+                                        @endforelse
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="card-footer">
-                    <div class="d-flex justify-content-center justify-content-sm-end">
-                    </div>
-                </div>
+                <!-- End Tab Content -->
             </div>
         </div>
+
+    </div>
     </div>
 @endsection
